5bf59fad0ff0b5065f7d6bb15f4e69f865e1baf5
Marcin Mielżyński <lopx@gazeta.pl>
2008-09-10 16:53:26 +0000

    1.9 doesn't use capture histories.
    git-svn-id: http://svn.codehaus.org/jruby/joni/trunk@7672 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/joni/Config.java
+++ src/org/joni/Config.java
@@ -35,7 +35,7 @@ public interface Config extends org.jcodings.Config {
     final boolean CASE_FOLD_IS_APPLIED_INSIDE_NEGATIVE_CCLASS = true;
     
     final boolean USE_MATCH_RANGE_MUST_BE_INSIDE_OF_SPECIFIED_RANGE = false;
-    final boolean USE_CAPTURE_HISTORY = true;
+    final boolean USE_CAPTURE_HISTORY = false;
     final boolean USE_VARIABLE_META_CHARS = true;
     final boolean USE_WORD_BEGIN_END = true;                                /* "\<": word-begin, "\>": word-end */
     final boolean USE_POSIX_API_REGION_OPTION = true;                           /* needed for POSIX API support */ 
