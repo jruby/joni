800d5ff28da5c99edada24ca0be133b67202c931
Charles Oliver Nutter <headius@headius.com>
2018-09-14 15:44:43 -0500

    Isolate JDK9 stuff and finish making modularization.

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ open module org.jruby.joni {
     exports org.joni;
     exports org.joni.constants;
     exports org.joni.exception;
-    
+
     requires org.jruby.jcodings;
+    requires org.objectweb.asm;
 }
